e11a64415585e1d1d9f5dc91523ea1ec53a7f892
Fitsbit <107160828+Fitsbit@users.noreply.github.com>
2023-02-24 12:54:08 -0800

    Add files via upload

---
+++ RealDdos.py
@@ -0,0 +1,20 @@
+print("\n")
+print("Coded By : Mr.BL4Z3, Modded by Fitsbit")
+print("Author   : T34m V18rs, mod by Fitsbit")
+print("Github   : github.com/T34mV18rs, https://github.com/Fitsbit")
+print("Fb Page  : facebook.com/TeamVirusOfficial, Modder doesn't have fb")
+print("FB Group : facebook.com/groups/mohinhossen, modder doesn't have fb")
+print("Telegram : t.me/Crackerspace modder doesn't have fb")
+print("Join Cracker Space TG Group To Get Premium Apk(s) Free modder doesn't have this")
+print("Note- This Tool An Illegal Tool & It's Only For Educational Purpose.. Use It At Your Own Risk,We'll Be Not Responsible For Kind Of Problemfs")
+print("Modder's Discord Server: https://discord.gg/qHbBZT6nMr")
+print("Modder's Discord username: The King's Crown#1034")
+print("\n")
+num1 = int(input("Enter ip "))
+num2 = int(input("Enter port "))
+while True:
+    
+    
+    # Perform some operation using the two integers
+    # For example, you could print their sum:
+    print(f"Sending packets to {num1} through port {num2}")
