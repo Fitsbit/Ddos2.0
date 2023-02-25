5710f9eb9453e6867e3a90d7ae60ef7c3ca4183b
Fitsbit <107160828+Fitsbit@users.noreply.github.com>
2023-02-24 18:01:02 -0800

    added colrs and stuff

--- RealDdos.py
+++ RealDdos.py
@@ -1,13 +1,65 @@
-print("\n")
-print("Coded By : Mr.BL4Z3, Modded by Fitsbit")
-print("Author   : T34m V18rs, mod by Fitsbit")
+# import time
+
+# print("\n")
+# print("Coded By : Mr.BL4Z3, Modded by Fitsbit")
+# print("Author   : T34m V18rs, mod by Fitsbit")
+# print("Github   : github.com/T34mV18rs, https://github.com/Fitsbit")
+# print("Fb Page  : facebook.com/TeamVirusOfficial, Modder doesn't have fb")
+# print("FB Group : facebook.com/groups/mohinhossen, modder doesn't have fb")
+# print("Telegram : t.me/Crackerspace modder doesn't have fb")
+# print("Join Cracker Space TG Group To Get Premium Apk(s) Free modder doesn't have this")
+# print("Note- This Tool An Illegal Tool & It's Only For Educational Purpose.. Use It At Your Own Risk,We'll Be Not Responsible For Kind Of Problemfs")
+# print("Modder's Discord Server: https://discord.gg/qHbBZT6nMr")
+# print("Modder's Discord username: The King's Crown#1034")
+# print("\n")
+# num1 = input("Enter ip: ")
+# num2 = int(input("Enter port: "))
+# print("WARNING, this is an illegal tool that can harm a person, do not do if you don't know what it is or what it can do.")
+# user_input = input("Do you want to continue? (y/n): ")
+# while user_input.lower() != 'y':
+#     if user_input.lower() == 'n':
+#         user_input = input("Do you want to exit? (y/n): ")
+#         if user_input.lower() == 'y':
+#             exit()
+#     user_input = input("Do you want to continue? (y/n):  ")
+# print("Get ready...")
+# print("[                    ] 0%")
+# time.sleep(1)
+# print("[=====               ] 25%")
+# time.sleep(1)
+# print("[==========          ] 50%")
+# time.sleep(1)
+# print("[===============     ] 75%")
+# time.sleep(1)
+# print("[====================] 100%")
+# time.sleep(1)
+
+# while True:
+#     print(f"Sending packets to {num1} through port {num2}")
+#     num2 += 1
+
+
+
 import sys
 import os
 import time
 import socket
 import random
 
-print("\n")
+# Define ANSI escape codes for colors
+GREEN = "\033[92m"
+RED = "\033[91;1m"
+RESET = "\033[0m"
+
+# Print header in green
+print(f"{GREEN}__     __    ____      _      ____  ")
+print("\\ \\   / /   |  _ \\  __| | ___/ ___| ")
+print(" \\ \\ / /____| | | |/ _` |/ _ \\___ \\ ")
+print("  \\ V /_____| |_| | (_| | (_) |__) |")
+print("   \\_/      |____/ \\__,_|\\___/____/ {RESET}")
+
+                                    
+print(f"{GREEN}\n")
 print("Coded By : Mr.BL4Z3, Modded by Fitsbit")
 print("Author   : T34m V18rs, mod by Fitsbit")
 print("Github   : github.com/T34mV18rs, https://github.com/Fitsbit")
@@ -18,18 +70,28 @@ print("Join Cracker Space TG Group To Get Premium Apk(s) Free modder doesn't hav
 print("Note- This Tool An Illegal Tool & It's Only For Educational Purpose.. Use It At Your Own Risk,We'll Be Not Responsible For Kind Of Problemfs")
 print("Modder's Discord Server: https://discord.gg/qHbBZT6nMr")
 print("Modder's Discord username: The King's Crown#1034")
-print("\n")
-num1 = input("Enter ip ")
-num2 = input("Enter port ")
-print("WARNING, this is an illegal tool that can harm a person, do not do if you don't know what it is or what it can do.")
-user_input = input("Do you want to continue? (y/n): ")
+print(f"{RESET}\n")
+
+# Get user input in red
+num1 = input(f"{RED}Enter ip: {RESET}")
+num2 = int(input(f"{RED}Enter port: {RESET}"))
+num3 = int(input(f"{RED}Enter how many bytes the attack should be, higher more powerful.: {RESET}"))
+
+
+# Print warning in red
+print(f"{RED}WARNING, this is an illegal tool that can harm a person, do not do if you don't know what it is or what it can do.{RESET}")
+
+# Get user confirmation in red
+user_input = input(f"{RED}Do you want to continue? (y/n): {RESET}")
 while user_input.lower() != 'y':
     if user_input.lower() == 'n':
-        user_input = input("Do you want to exit? (y/n): ")
+        user_input = input(f"{RED}Do you want to exit? (y/n): {RESET}")
         if user_input.lower() == 'y':
             exit()
-    user_input = input("Do you want to continue? (y/n):  ")
-print("Get ready...")
+    user_input = input(f"{RED}Do you want to continue? (y/n): {RESET}")
+    
+# Print progress messages in green
+print(f"{GREEN}Get ready...")
 print("[                    ] 0%")
 time.sleep(1)
 print("[=====               ] 25%")
@@ -40,12 +102,114 @@ print("[===============     ] 75%")
 time.sleep(1)
 print("[====================] 100%")
 time.sleep(1)
+num = 0
+
+# Send packets to IP address using increasing port numbers
 while True:
+    print(f"{GREEN}Sending {num3} packets to {num1} through port {num2}       Times sent: {num}       ")
+    num2 = num2+1 
+    num = num+1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
     
-    # Perform some operation using the two integers
-    # For example, you could print their sum:
-    print(f"Sending packets to {num1} through port {num2}")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # print("\n")
+# print("Coded By : Mr.BL4Z3, Modded by Fitsbit")
+# print("Author   : T34m V18rs, mod by Fitsbit")
+# import sys
+# import os
+# import time
+# import socket
+# import random
+
+# print("\n")
+# print("Coded By : Mr.BL4Z3, Modded by Fitsbit")
+# print("Author   : T34m V18rs, mod by Fitsbit")
+# print("Github   : github.com/T34mV18rs, https://github.com/Fitsbit")
+# print("Fb Page  : facebook.com/TeamVirusOfficial, Modder doesn't have fb")
+# print("FB Group : facebook.com/groups/mohinhossen, modder doesn't have fb")
+# print("Telegram : t.me/Crackerspace modder doesn't have fb")
+# print("Join Cracker Space TG Group To Get Premium Apk(s) Free modder doesn't have this")
+# print("Note- This Tool An Illegal Tool & It's Only For Educational Purpose.. Use It At Your Own Risk,We'll Be Not Responsible For Kind Of Problemfs")
+# print("Modder's Discord Server: https://discord.gg/qHbBZT6nMr")
+# print("Modder's Discord username: The King's Crown#1034")
+# print("\n")
+# num1 = input("Enter ip ")
+# num2 = input("Enter port ")
+# print("WARNING, this is an illegal tool that can harm a person, do not do if you don't know what it is or what it can do.")
+# user_input = input("Do you want to continue? (y/n): ")
+# while user_input.lower() != 'y':
+#     if user_input.lower() == 'n':
+#         user_input = input("Do you want to exit? (y/n): ")
+#         if user_input.lower() == 'y':
+#             exit()
+#     user_input = input("Do you want to continue? (y/n):  ")
+# print("Get ready...")
+# print("[                    ] 0%")
+# time.sleep(1)
+# print("[=====               ] 25%")
+# time.sleep(1)
+# print("[==========          ] 50%")
+# time.sleep(1)
+# print("[===============     ] 75%")
+# time.sleep(1)
+# print("[====================] 100%")
+# time.sleep(1)
+# while True:
+    
+    
+#     # Perform some operation using the two integers
+#     # For example, you could print their sum:
+#     print(f"Sending packets to {num1} through port {num2}")
 
 
 
