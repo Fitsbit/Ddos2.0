68c501777720da1019deb7f4d27a727e8def0982
Fitsbit <107160828+Fitsbit@users.noreply.github.com>
2023-02-24 17:19:26 -0800

    Added warning screen

--- RealDdos.py
+++ RealDdos.py
@@ -10,11 +10,53 @@ print("Note- This Tool An Illegal Tool & It's Only For Educational Purpose.. Use
 print("Modder's Discord Server: https://discord.gg/qHbBZT6nMr")
 print("Modder's Discord username: The King's Crown#1034")
 print("\n")
-num1 = int(input("Enter ip "))
-num2 = int(input("Enter port "))
+num1 = input("Enter ip ")
+num2 = input("Enter port ")
+print("WARNING, this is an illegal tool that can harm a person, do not do if you don't know what it is or what it can do.")
+user_input = input("Do you want to continue? (y/n): ")
+while user_input.lower() != 'y':
+    if user_input.lower() == 'n':
+        user_input = input("Do you want to exit? (y/n): ")
+        if user_input.lower() == 'y':
+            exit()
+    user_input = input("Do you want to continue? (y/n): (weird thing here, itll just  continue either way.) ")
+
+ 
 while True:
     
     
     # Perform some operation using the two integers
     # For example, you could print their sum:
     print(f"Sending packets to {num1} through port {num2}")
+
+    
+    
+    
+    
+
+
+
+
+
+
+#old version
+# print("\n")
+# print("Coded By : Mr.BL4Z3, Modded by Fitsbit")
+# print("Author   : T34m V18rs, mod by Fitsbit")
+# print("Github   : github.com/T34mV18rs, https://github.com/Fitsbit")
+# print("Fb Page  : facebook.com/TeamVirusOfficial, Modder doesn't have fb")
+# print("FB Group : facebook.com/groups/mohinhossen, modder doesn't have fb")
+# print("Telegram : t.me/Crackerspace modder doesn't have fb")
+# print("Join Cracker Space TG Group To Get Premium Apk(s) Free modder doesn't have this")
+# print("Note- This Tool An Illegal Tool & It's Only For Educational Purpose.. Use It At Your Own Risk,We'll Be Not Responsible For Kind Of Problemfs")
+# print("Modder's Discord Server: https://discord.gg/qHbBZT6nMr")
+# print("Modder's Discord username: The King's Crown#1034")
+# print("\n")
+# num1 = int(input("Enter ip "))
+# num2 = int(input("Enter port "))
+# while True:
+    
+    
+#     # Perform some operation using the two integers
+#     # For example, you could print their sum:
+#     print(f"Sending packets to {num1} through port {num2}")
