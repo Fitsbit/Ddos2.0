df3a9fbf3ca5730dcb5236194454fb44cc4d2de9
Fitsbit <107160828+Fitsbit@users.noreply.github.com>
2023-02-24 17:26:35 -0800

    added loaing screen

--- RealDdos.py
+++ RealDdos.py
@@ -20,14 +20,83 @@ while user_input.lower() != 'y':
         if user_input.lower() == 'y':
             exit()
     user_input = input("Do you want to continue? (y/n):  ")
-
- 
+print("Get ready...")
+ print("[                    ] 0%")
+time.sleep(1)
+print("[=====               ] 25%")
+time.sleep(1)
+print("[==========          ] 50%")
+time.sleep(1)
+print("[===============     ] 75%")
+time.sleep(1)
+print("[====================] 100%")
+time.sleep(1)
 while True:
     
     
     # Perform some operation using the two integers
     # For example, you could print their sum:
     print(f"Sending packets to {num1} through port {num2}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# old version
+# print("\n")
+# print("Coded By : Mr.BL4Z3, Modded by Fitsbit")
+# print("Author   : T34m V18rs, mod by Fitsbit")
+# print("Github   : github.com/T34mV18rs, https://github.com/Fitsbit")
+# print("Fb Page  : facebook.com/TeamVirusOfficial, Modder doesn't have fb")
+# print("FB Group : facebook.com/groups/mohinhossen, modder doesn't have fb")
+# print("Telegram : t.me/Crackerspace modder doesn't have fb")
+# print("Join Cracker Space TG Group To Get Premium Apk(s) Free modder doesn't have this")
+# print("Note- This Tool An Illegal Tool & It's Only For Educational Purpose.. Use It At Your Own Risk,We'll Be Not Responsible For Kind Of Problemfs")
+# print("Modder's Discord Server: https://discord.gg/qHbBZT6nMr")
+# print("Modder's Discord username: The King's Crown#1034")
+# print("\n")
+# num1 = input("Enter ip ")
+# num2 = input("Enter port ")
+# print("WARNING, this is an illegal tool that can harm a person, do not do if you don't know what it is or what it can do.")
+# user_input = input("Do you want to continue? (y/n): ")
+# while user_input.lower() != 'y':
+#     if user_input.lower() == 'n':
+#         user_input = input("Do you want to exit? (y/n): ")
+#         if user_input.lower() == 'y':
+#             exit()
+#     user_input = input("Do you want to continue? (y/n):  ")
+
+ 
+# while True:
+    
+    
+#     # Perform some operation using the two integers
+#     # For example, you could print their sum:
+#     print(f"Sending packets to {num1} through port {num2}")
 
     
     
